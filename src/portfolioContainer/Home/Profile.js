16ec4e23520c6f94001ce8fcec7c15fc475efe28
Mohammad Taghavi far <starsofchance@gmail.com>
2023-08-19 23:42:46 +0330

    initial commit, starting the build from the home components

---
+++ src/portfolioContainer/Home/Profile.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="profile-colz">
+            <a
+              href="https://github.com/starsofchance"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/muhammad-taghavi-far/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="fa-brands fa-linkedin-in"></i>
+            </a>
+            <a
+              href="https://twitter.com/miim_noon1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="fa-brands fa-twitter"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/ultimat3.shooter/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
